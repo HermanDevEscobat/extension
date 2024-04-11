8240b309f5eaca2e6e694a7e4c3cf632632f04b9
Herman <gaksonov@mail.ru>
2024-04-11 21:48:30 +0300

    modfication filter shk

--- background.js
+++ background.js
@@ -18,7 +18,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [1, 2],
+        removeRuleIds: [1, 2, 3],
         addRules: [
             {
                 id: 1,
@@ -35,9 +35,19 @@ chrome.runtime.onInstalled.addListener(() => {
             },
             {
                 id: 2,
-                priority: 2,
+                priority: 1,
                 condition: {
-                    regexFilter: "^http://.*:1100/api/boxes/accept\\?boxShk=.*;.*:.*:(?!2855$)"
+                    regexFilter: "^http://.*:1100/api/boxes/accept\\?boxShk=.*:.*:.*:2855"
+                },
+                action: {
+                    type: "allow"
+                },
+            },
+            {
+                id: 3,
+                priority: 1,
+                condition: {
+                    regexFilter: "^http://.*:1100/api/boxes/accept\\?boxShk=.*:.*:.*:.*"
                 },
                 action: {
                     type: "redirect",
@@ -49,7 +59,26 @@ chrome.runtime.onInstalled.addListener(() => {
         ],
     });
 });
-
+// Образец повтора:                     http://10.54.81.254:1100/api/wavebreaker/put?shk=*BCEpRgED&currentCell=undefined&forcePut=false&lastBarcode=undefined
+// Приемка неизвестного товара:         http://10.54.81.254:1100/api/wavebreaker/put?shk=*%D0%98%D0%A3%D0%90%D0%A9%D0%AB99%D0%A3&currentCell=null&forcePut=false&lastBarcode=undefined
+// Повторный прием неизвестного товара: http://10.54.81.254:1100/api/wavebreaker/put?shk=*%D0%98%D0%A3%D0%90%D0%A9%D0%AB99%D0%A3&currentCell=464&forcePut=false&lastBarcode=undefined
+// Принят новый ШК через ручной ввод:   http://10.54.81.254:1100/api/wavebreaker/put?shk=19072263149&currentCell=undefined&forcePut=false&lastBarcode=undefined
+// Ошибка:                              http://10.54.81.254:1100/api/wavebreaker/put?shk=%E2%84%96%D1%86%D1%8801%D0%B6776507%D0%B62038557795641%D0%B6660426%D0%B0%D0%B8%D1%84%D1%848%D1%843&currentCell=null&forcePut=false&lastBarcode=undefined
+// Ответ ошибки: {
+//     "cellId": 0,
+//     "userId": null,
+//     "result": 1,
+//     "msg": "Не удалось распознать ШК",
+//     "acceptedCount": 0,
+//     "expectedCount": 0,
+//     "placeName": null,
+//     "userName": null,
+//     "clientName": null,
+//     "clientInOffice": false,
+//     "needExcise": false,
+//     "shkId": 0,
+//     "prevLocation": null
+// }
 // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 //     if (message.action === "addRedirectRule") {
 //       addRedirectRule();
